3c28546ff4db347dee27a15a11e365f96884f7f7
Puyma <puyma@amyup.xyz>
2022-07-11 07:22:41 +0200

    bzero to use memset

--- ft_bzero.c
+++ ft_bzero.c
@@ -14,19 +14,5 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	unsigned char	*src;
-
-	src = s;
-	while (n)
-	{
-		*src++ = '\0';
-		--n;
-	}
-}
-
-/*
-void	ft_bzero(void *s, size_t n)
-{
 	ft_memset(s, 0, n);
 }
-*/
