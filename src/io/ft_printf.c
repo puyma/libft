c99f33a01d52f20b25199e994e39def43871f8e2
puyma <mpuig-ma@student.42barcelona.com>
2022-08-13 12:04:46 +0200

    Error: Unrecognized line (27, 41) while parsing line [<BWISE_AND>]

--- src/io/ft_printf.c
+++ src/io/ft_printf.c
@@ -15,7 +15,6 @@
 int	ft_printf(const char *format, ...)
 {
 	static const char	formats[] = "cspdiuxX%";
-	t_fptr				func;
 	t_printout			p;
 
 	va_start(p.args, format);
@@ -24,9 +23,8 @@ int	ft_printf(const char *format, ...)
 	{
 		if (*format == '%' && ft_strchr(formats, *(format + 1)) != 0)
 		{
-			func = ft_formats(format + 1);
-			func(&p.args, &p, format + 1);
 			format++;
+			ft_formats(format)(&p.args, &p, format);
 		}
 		else if (*format == '%' && ft_strchr(formats, *(format + 1) == 0))
 			ft_putstr("Invalid format");
@@ -42,3 +40,12 @@ int	ft_printf(const char *format, ...)
 	va_end(p.args);
 	return (p.n_written);
 }
+
+/*
+ * 	t_fptr	func;
+ *
+ * 	func = ft_formats(format);
+ * 	func(&p.args, &p, format);
+ *
+ */
+
