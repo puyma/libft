b693ca4903b73f9313115e2d30c073d056e2749b
puyma <mpuig-ma@student.42barcelona.com>
2022-09-11 19:09:51 +0200

    saving

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 20:29:35 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2022/09/11 15:06:53 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2022/09/11 19:09:06 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,26 +48,32 @@ char	*ft_read_until(int fd)
 		//ft_putstr_mod("--: ");ft_putstr_mod(buf);ft_putstr_mod("  :--");
 		//write(1, "\n", 1);
 	}
-	if (read_return <= -1)
-		write(1, "Errored reading file descriptor.\n", 33);
-	else if (read_return == 0)
-		write(1, "Reached end of file.\n", 23);
+	//if (read_return <= -1)
+	//	write(1, "Errored reading file descriptor.\n", 33);
+	//else if (read_return == 0)
+	//	write(1, "Reached end of file.\n", 23);
 	//write(1, "end> ft_read_until()\n", 21);
-
-	ft_putstr_mod("w_buffer> \"");
-	ft_putstr_mod(whole_buffer);
-	write(1, "\"\n", 2);
-
+	//ft_putstr_mod("w_buffer> \"");
+	//ft_putstr_mod(whole_buffer);
+	//write(1, "\"\n", 2);
+	if (read_return == -1)
+	{
+		if (buf != NULL)
+			free(buf);
+		return (NULL);
+	}
 	free(buf);
 	return (whole_buffer);
 }
 
-char	*ft_get_line(const char *buf, char **remains)
+char	*ft_get_line(const char *buffer, char **remains)
 {
 	char	*line;
 	char	*substr;
 
-	substr = ft_substr(buf, 0, ft_strchr(buf, '\n') - buf);
+	if (buffer == NULL)
+		return (NULL);
+	substr = ft_substr(buffer, 0, ft_strchr(buffer, '\n') - buffer + 1);
 	if (*remains == NULL)
 		line = substr;
 	else
@@ -75,7 +81,7 @@ char	*ft_get_line(const char *buf, char **remains)
 		line = ft_strjoin(*remains, substr);
 		free(*remains);
 	}
-	*remains = ft_substr(buf, ft_strchr(buf, '\n') - buf + 1, ft_strlen(buf));
+	*remains = ft_substr(buffer, ft_strchr(buffer, '\n') - buffer + 1, ft_strlen(buffer));
 	return (line);
 }
 
@@ -86,10 +92,12 @@ char	*get_next_line(int fd)
 	static char	*leftovers;
 
 	buffer = ft_read_until(fd);
-	if (ft_strchr(buffer, '\n') == 0)
-	{
-		write(1, "Something went wrong when trying to ft_get_line.\n", 49);
+	if (buffer == NULL)
 		return (NULL);
+	if (buffer != NULL && ft_strchr(buffer, '\n') == 0)
+	{
+	//	write(1, "Something went wrong when trying to ft_get_line.\n", 49);
+		//return (NULL);
 	}
 	line = ft_get_line(buffer, &leftovers);
 	free(buffer);
