46553bdaea6cb36aa29b6d86da74c2e6caa095d4
puyma <mpuig-ma@student.42barcelona.com>
2022-09-27 20:18:25 +0200

    ft_isspace

---
+++ src/ctype/ft_isspace.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isspace.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/27 19:51:20 by mpuig-ma          #+#    #+#             */
+/*   Updated: 2022/09/27 20:10:06 by mpuig-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** The isspace() function tests for the white-space characters. 
+** For any locale, this includes the following standard characters:
+** ``\t''   ``\n''    ``\v''    ``\f''    ``\r''    `` ''
+**
+** The isspace() function returns zero if the character tests false 
+** and returns non-zero if the character tests true.
+*/
+
+int	ft_isspace(int c)
+{
+	if (('\t' <= c && c <= '\r') || c == ' ')
+		return (0);
+	return (1);
+}
