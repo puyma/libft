f91b1baec8541ec389d6606dbb894707853daea6
puyma <mpuig-ma@student.42barcelona.com>
2022-10-07 21:55:37 +0200

    added commented explanation

--- src/ctype/ft_isalnum.c
+++ src/ctype/ft_isalnum.c
@@ -6,9 +6,18 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 18:26:39 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2022/05/09 18:36:04 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2022/10/03 20:34:57 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+** The isalnum() function tests for any character for which isalpha(3) 
+** or isdigit(3) is true.  The value of the argument must be representable 
+** as an unsigned char or the value of EOF.
+** 
+** The isalnum() function returns zero if the character tests false 
+** and returns non-zero if the character tests true.
+*/
 
 #include "ft_ctype.h"
 
