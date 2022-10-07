6e517a775d7c027f72c23b3f3322ecab2fce1725
puyma <mpuig-ma@student.42barcelona.com>
2022-10-07 21:56:51 +0200

    fn description && numbers to ascii representation

--- src/ctype/ft_isascii.c
+++ src/ctype/ft_isascii.c
@@ -6,15 +6,20 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 18:37:07 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2022/05/09 18:38:00 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2022/10/03 20:38:35 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+** The isascii() function tests for and ASCII character, which is any character
+** between 0 and octal 0177 inclusive. 
+*/
 
 #include "ft_ctype.h"
 
 int	ft_isascii(int c)
 {
-	if (0 <= c && c <= 127)
+	if (0 <= c && c <= 0177)
 		return (1);
 	return (0);
 }
