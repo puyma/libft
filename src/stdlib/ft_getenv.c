4ebeada6af3a90832a2f7be4f71e8e00c1d2f706
puyma <mpuig-ma@student.42barcelona.com>
2023-06-07 17:21:47 +0200

    fix to comply with original func

--- src/stdlib/ft_getenv.c
+++ src/stdlib/ft_getenv.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 16:11:07 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/06/02 16:52:08 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/07 16:57:00 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 char	*ft_getenv(const char *name, const char **env)
 {
 	char	*variable;
+	size_t	variable_len;
 	int		i;
 
 	i = 0;
 	variable = ft_strjoin(name, "=");
+	variable_len = ft_strlen(variable);
 	while (env[i] != NULL)
 	{
-		if (ft_strncmp(env[i], variable, ft_strlen(variable)) == 0)
+		if (ft_strncmp(env[i], variable, variable_len) == 0)
 		{
 			free(variable);
-			return ((char *) env[i]);
+			return ((char *) env[i] + variable_len);
 		}
 		++i;
 	}
