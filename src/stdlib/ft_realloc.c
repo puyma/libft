e4fd249d683bd26b97fd241dbd3d5d176a1a1d61
puyma <mpuig-ma@student.42barcelona.com>
2023-06-13 16:18:20 +0200

    ok...?

--- src/stdlib/ft_realloc.c
+++ src/stdlib/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 15:39:40 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/06/13 15:42:22 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/13 16:11:58 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ void	*ft_realloc(void *ptr, size_t size)
 	void	*realloc_mem;
 
 	realloc_mem = NULL;
-	realloc_mem = realloc(ptr, size);
+	realloc_mem = malloc(size);
+	if (realloc_mem == NULL)
+		return (NULL);
+	realloc_mem = ft_memcpy(realloc_mem, ptr, size);
+	//realloc_mem = realloc(ptr, size);
 	return (realloc_mem);
 }
